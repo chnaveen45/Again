847723eb8e29822736eef9cf4422f6a8831849cf
Chinthala naveen <Chinthalanaveen504@gmail.com>
2024-12-06 10:16:58 +0530

    bug in service is fixed

--- src/Navbar/Custom.js
+++ src/Navbar/Custom.js
@@ -44,10 +44,10 @@ function Custom() {
         </div>
 
       </div>
-  
+
       <div className="company-container">
         <div className="company-text">
-          <h1 style={{color:'#0000B3',fontFamily:'Josefin Sans'}}><b>Our Company</b></h1>
+          <h1 style={{ color: '#0000B3', fontFamily: 'Josefin Sans' }}><b>Our Company</b></h1>
           <p>
             Welcome to Vishwasri Technologies Pvt Ltd, a dynamic startup dedicated to crafting innovative
             software solutions tailored to meet the diverse needs of modern businesses.
@@ -69,7 +69,7 @@ function Custom() {
       </div>
 
       <div>
-        <h1 style={{ display: 'flex', justifyContent: 'center', color:'#0000B3',fontFamily:'Inria Serif',marginTop:'-3%'}}><b>Services</b></h1>
+        <h1 style={{ display: 'flex', justifyContent: 'center', color: '#0000B3', fontFamily: 'Inria Serif', marginTop: '-3%' }}><b>Services</b></h1>
         <div className="mm">
 
 
@@ -79,7 +79,7 @@ function Custom() {
               <Card.Img variant="top" src={card5} />
               <Card.Body style={{ background: '#014CA6', color: 'white', }}>
                 <Card.Title>Graphic Design</Card.Title>
-                <Card.Text>Our graphic design team brings creativity and professionalism to every project. We offer a range of design services, including branding, logo creation, social media graphic and marketing materials. Our goal is to help your business make a lasting visual impression that resonates with tour audience. <Link to="/servies" style={{ color: 'red' }}>  Know more</Link></Card.Text>
+                <Card.Text>Our graphic design team brings creativity and professionalism to every project. We offer a range of design services, including branding, logo creation, social media graphic and marketing materials. Our goal is to help your business make a lasting visual impression that resonates with tour audience. <Link to="/services" style={{ color: 'red' }}>  Know more</Link></Card.Text>
 
               </Card.Body>
             </Card>
@@ -89,7 +89,7 @@ function Custom() {
               <Card.Img variant="top" src={card6} />
               <Card.Body style={{ background: '#014CA6', color: 'white', }}>
                 <Card.Title>Web & App Development</Card.Title>
-                <Card.Text>With a focus on functionality and user experience, our web development team creates responsive, secure, and visually appealing websites. From e-commerce platforms to corporate websites, we ensure that each site reflects the brand’s identity while meeting <Link to="/servies" style={{ color: 'red' }}>  Know more</Link> </Card.Text>
+                <Card.Text>With a focus on functionality and user experience, our web development team creates responsive, secure, and visually appealing websites. From e-commerce platforms to corporate websites, we ensure that each site reflects the brand’s identity while meeting <Link to="/services" style={{ color: 'red' }}>  Know more</Link> </Card.Text>
 
               </Card.Body>
             </Card>
@@ -99,7 +99,7 @@ function Custom() {
               <Card.Img variant="top" src={card7} />
               <Card.Body style={{ background: '#014CA6', color: 'white', }}>
                 <Card.Title>Marketing</Card.Title>
-                <Card.Text>Our marketing services are designed to help your business grow in a digital-first world. We offer comprehensive strategies across social media marketing, search engine optimization (SEO), content marketing and paid advertising, helping to increase brand visibility, reach target audiences. <Link to="/servies" style={{ color: 'red' }}>  Know more</Link> </Card.Text>
+                <Card.Text>Our marketing services are designed to help your business grow in a digital-first world. We offer comprehensive strategies across social media marketing, search engine optimization (SEO), content marketing and paid advertising, helping to increase brand visibility, reach target audiences. <Link to="/services" style={{ color: 'red' }}>  Know more</Link> </Card.Text>
 
               </Card.Body>
             </Card>
@@ -111,7 +111,7 @@ function Custom() {
 
 
       <div>
-        <h2 style={{ display: 'flex', justifyContent: 'center', marginTop: '4%', marginBottom: '4%',color:'#0000B3',fontFamily:'Inria Serif', }}><b>Development Process</b></h2>
+        <h2 style={{ display: 'flex', justifyContent: 'center', marginTop: '4%', marginBottom: '4%', color: '#0000B3', fontFamily: 'Inria Serif', }}><b>Development Process</b></h2>
         <div className="development-process">
           <div className="text-content">
 
@@ -160,76 +160,76 @@ function Custom() {
       </div>
 
 
- <h1 class="title-section"style={{ display: 'flex', justifyContent: 'center', marginTop: '4%', marginBottom: '4%',color:'#0000B3',fontFamily:'Inria Serif', }} ><b>Why Choose Us</b></h1>
-<div class="why-choose-us-container">
- 
-  <div class="content-wrapper">
-    <div class="images-section">
-      <img src={card9} alt="Office space" class="image top-image" />
-      <img src={card10} alt="Team collaboration" class="image bottom-image" />
-    </div>
-    <div class="content-section">
-      <p>
-        We are committed to delivering scalable solutions that are adaptable
-        and future-ready, ensuring our clients stay prepared for evolving
-        business landscapes. With an agile development approach, we embrace
-        flexibility and iterative processes to deliver efficient and timely
-        results. Our customer-centric philosophy focuses on tailoring solutions
-        to meet each client's unique needs and goals.
-      </p>
-    </div>
-  </div>
-</div>
+      <h1 class="title-section" style={{ display: 'flex', justifyContent: 'center', marginTop: '4%', marginBottom: '4%', color: '#0000B3', fontFamily: 'Inria Serif', }} ><b>Why Choose Us</b></h1>
+      <div class="why-choose-us-container">
 
-<div>
-
-
-   <footer className="footer" style={{ display: 'flex', justifyContent: 'center', marginTop: '4%',}}>
-  <div className="footer-content">
-    <div className="footer-section">
-      <h4>About Us</h4>
-      <Link to="/services#web-development">Web Development</Link><br />
-      <Link to="/services#app-development">App Development</Link><br />
-      <Link to="/services#graphic-design">Graphic Design</Link><br />
-      <Link to="/services#marketing">Marketing</Link>
-      <div className="social-icons">
-       <h4>Follow Us</h4>
-        <a href="https://www.instagram.com/vishwasri_technologies_01/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href="https://www.youtube.com/@VISHWASRITECHNOLOGIES" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} /></a>
-        <a href="https://www.linkedin.com/in/vishwasri-technologies-330005331/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="https://www.vishcom.net" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGoogle} /></a>
+        <div class="content-wrapper">
+          <div class="images-section">
+            <img src={card9} alt="Office space" class="image top-image" />
+            <img src={card10} alt="Team collaboration" class="image bottom-image" />
+          </div>
+          <div class="content-section">
+            <p>
+              We are committed to delivering scalable solutions that are adaptable
+              and future-ready, ensuring our clients stay prepared for evolving
+              business landscapes. With an agile development approach, we embrace
+              flexibility and iterative processes to deliver efficient and timely
+              results. Our customer-centric philosophy focuses on tailoring solutions
+              to meet each client's unique needs and goals.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="footer-section">
-      <h4>Industries we serve</h4>
-      <a href="#ecommerce"><p>E-commerce</p></a>
-      <a href="#finance"><p>Finance</p></a>
-      <a href="#education"><p>Education</p></a>
-      <a href="#healthcare"><p>Health care</p></a>
-      <a href="#retail"><p>Retail</p></a>
-    </div>
-    <div className="footer-section">
-      <h4>Contact Us</h4>
-      <p><FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:VISHWASRITECHNOLOGIES@vishcom.net">VISHWASRITECHNOLOGIES@vishcom.net</a></p>
-      <p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+91 7330669716">+91 7330669716</a></p>
-      <p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+91 9908208857">+91 9908208857</a></p>
-    </div>
-    <div className="footer-section">
-      <h4>Located At</h4>
-      <div className="location-info">
-        <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
-        <a href="https://www.google.com/maps/place/Vishwasri+Technologies+Pvt.Ltd/@17.4442944,78.4634537,99m/data=!3m1!1e3!4m12!1m5!3m4!2zMTfCsDI2JzM4LjQiTiA3OMKwMjcnNDguMSJF!8m2!3d17.444!4d78.4633611!3m5!1s0x3bcb9100083e8389:0xceb1ec1986df69c8!8m2!3d17.4441096!4d78.4633524!16s%2Fg%2F11wwpw6zvb?entry=ttu&g_ep=EgoyMDI0MTIwMi4wIKXMDSoASAFQAw%3D%3D">1-10-74/B&C FLAT NO: T-402/B, TECHNOPOLIS GALADA COMPLEX, DWARAKA DAS COLONY, BEGUMPET 500016</a>
+
+      <div>
+
+
+        <footer className="footer" style={{ display: 'flex', justifyContent: 'center', marginTop: '4%', }}>
+          <div className="footer-content">
+            <div className="footer-section">
+              <h4>About Us</h4>
+              <Link to="/services#web-development">Web Development</Link><br />
+              <Link to="/services#app-development">App Development</Link><br />
+              <Link to="/services#graphic-design">Graphic Design</Link><br />
+              <Link to="/services#marketing">Marketing</Link>
+              <div className="social-icons">
+                <h4>Follow Us</h4>
+                <a href="https://www.instagram.com/vishwasri_technologies_01/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href="https://www.youtube.com/@VISHWASRITECHNOLOGIES" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} /></a>
+                <a href="https://www.linkedin.com/in/vishwasri-technologies-330005331/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href="https://www.vishcom.net" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGoogle} /></a>
+              </div>
+            </div>
+            <div className="footer-section">
+              <h4>Industries we serve</h4>
+              <a href="#ecommerce"><p>E-commerce</p></a>
+              <a href="#finance"><p>Finance</p></a>
+              <a href="#education"><p>Education</p></a>
+              <a href="#healthcare"><p>Health care</p></a>
+              <a href="#retail"><p>Retail</p></a>
+            </div>
+            <div className="footer-section">
+              <h4>Contact Us</h4>
+              <p><FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:VISHWASRITECHNOLOGIES@vishcom.net">VISHWASRITECHNOLOGIES@vishcom.net</a></p>
+              <p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+91 7330669716">+91 7330669716</a></p>
+              <p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+91 9908208857">+91 9908208857</a></p>
+            </div>
+            <div className="footer-section">
+              <h4>Located At</h4>
+              <div className="location-info">
+                <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
+                <a href="https://www.google.com/maps/place/Vishwasri+Technologies+Pvt.Ltd/@17.4442944,78.4634537,99m/data=!3m1!1e3!4m12!1m5!3m4!2zMTfCsDI2JzM4LjQiTiA3OMKwMjcnNDguMSJF!8m2!3d17.444!4d78.4633611!3m5!1s0x3bcb9100083e8389:0xceb1ec1986df69c8!8m2!3d17.4441096!4d78.4633524!16s%2Fg%2F11wwpw6zvb?entry=ttu&g_ep=EgoyMDI0MTIwMi4wIKXMDSoASAFQAw%3D%3D">1-10-74/B&C FLAT NO: T-402/B, TECHNOPOLIS GALADA COMPLEX, DWARAKA DAS COLONY, BEGUMPET 500016</a>
+              </div>
+
+              <div className="footer-image">
+                <img src={card11} alt="loading..." />
+              </div>
+            </div>
+          </div>
+
+        </footer>
+
       </div>
-    
-      <div className="footer-image">
-    <img src={card11} alt="loading..." />
-  </div>
-    </div>
-  </div>
-
-</footer>
-
-</div>
     </div>
 
 
